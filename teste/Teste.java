9640e3c909e883bd439a3c9bc09bf6baceb6ab5c
Jorge Meireles <jorgentoniomeireles@gmail.com>
2021-12-18 09:55:00 -0300

    Criação das classes campo e tabuleiro  e criação do pacote para teste com JUnit5

---
+++ teste/Teste.java
@@ -0,0 +1,22 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testar() {
+		int a = 1 + 1;
+		assertEquals(2, a);
+	}
+	
+	
+	@Test
+	void testearSeIgualATres() {
+		int x =  2 + 1;
+		assertEquals(3, x);
+	}
+
+}
