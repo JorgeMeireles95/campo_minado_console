0c73cdb81e5d29983ab1bb4a108302d70d441647
Jorge Meireles <jorgentoniomeireles@gmail.com>
2021-12-20 17:24:30 -0300

    finalizando classe Campo

--- src/br/com/cod3r/com/modelo/Campo.java
+++ src/br/com/cod3r/com/modelo/Campo.java
@@ -43,7 +43,6 @@ public class Campo {
 			return false;
 		}
 	}
-	
 
 	void alternarMarcacao() {
 		if (!aberto) {
@@ -51,7 +50,6 @@ public class Campo {
 
 		}
 	}
-	
 
 	boolean abrir() {
 
@@ -68,36 +66,71 @@ public class Campo {
 			}
 			return true;
 
-		}else {
+		} else {
 			return false;
 		}
 
-	
 	}
 
-	
 	boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 
 	}
-	
-	
-	void  minar() {
+
+	void minar() {
 		minado = true;
 	}
-	
-	
+
 	public boolean isMarcado() {
 		return marcado;
 	}
-	
 
 	public boolean isAberto() {
 		return aberto;
 	}
+
+	public boolean isFechado() {
+		return !aberto;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+
+	long minasNaVizinhanca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+
+	}
+
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
 	
 	
-	
-	
+	public String toString() {
+		if(marcado) {
+			return "X";
+		}else if(aberto && minado ) {
+			return "*";
+		}else if(aberto && minasNaVizinhanca()>0) {
+			return Long.toString(minasNaVizinhanca());
+		}else if(aberto) {
+			return " ";
+		}else {
+			return "?";
+		}
+	}
 
 }
